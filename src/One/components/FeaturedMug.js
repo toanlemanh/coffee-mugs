361a10a65f10ea972bb0d4dd582d7cd1f55b0bd2
toanlemanh <toanlemanh2003@gmail.com>
2024-05-27 14:50:48 +0700

    complete feature-mug

--- src/One/components/FeaturedMug.js
+++ src/One/components/FeaturedMug.js
@@ -5,18 +5,62 @@ function FeaturedMug() {
         className="uppercase text-gray-400 text-center font-Karla text-sm text-[12px]
             "
       >
-        <span className="my-label-line"></span>{" "}
-        Featured Mugs{" "}
+        <span className="my-label-line"></span> Featured Mugs{" "}
         <span className="my-label-line"></span>
       </div>
       <div className="flex max-md:flex-col max-md:mx-20 md:justify-center md:mx-auto gap-x-10 gap-y-10 py-10">
         <div>
-          <div className="h-[450px] md:w-96 max-md:w-48 bg-no-repeat bg-center bg-[url('https://assets.website-files.com/5be96251aaba7a84f6ecdf81/5be96251aaba7a512bece011_liana-mikah-698524-unsplash.jpg')]"></div>
+          <div className="h-[450px] md:w-96 max-md:w-48 bg-no-repeat bg-center bg-[url('https://assets.website-files.com/5be96251aaba7a84f6ecdf81/5be96251aaba7a512bece011_liana-mikah-698524-unsplash.jpg')]">
+            <a href="#" className="cursor-pointer no-underline">
+              <div className="h-[100%] hover:bg-opacity-15 hover:bg-gray-500 relative md:w-96 max-md:w-48">
+                <div
+                  className="absolute tracking-wider text-sm bg-white px-4 py-2 font-bold text-center 
+              top-3 right-3 hover:bg-stone-500 cursor-pointer max-md:text-sm"
+                >
+                  On sale.
+                </div>
+                <div className="absolute bottom-4 right-3 left-3 bg-slate-50 px-4 py-2 shadow-sm uppercase text-center hover:bg-stone-500 cursor-pointer max-md:text-sm">
+                  Expand our product
+                </div>
+              </div>
+            </a>
+          </div>
           <h3 className=" cursor-pointer md:text-center">
-             <a className="text-slate-500 font-semi-bold text-base">Pink Preminum Ceramic</a>
-             <p className="text-slate-500 font-thin text-sm max-md:px-16">$23.00</p>
+            <a className="text-slate-500 font-semi-bold text-base ">
+              Pink Preminum Ceramic
+            </a>
+            <p className="text-slate-500 font-thin text-sm max-md:px-16 max-md:text-sm">
+              $23.00
+            </p>
           </h3>
         </div>
+        {/* End feature 1 */}
+        <div>
+          <div className="h-[450px] md:w-96 max-md:w-48 bg-no-repeat bg-center bg-[url('https://assets.website-files.com/5be96251aaba7a84f6ecdf81/5be96251aaba7a512bece011_liana-mikah-698524-unsplash.jpg')]">
+            <a href="#" className="cursor-pointer no-underline">
+              <div className="h-[100%] hover:bg-opacity-15 hover:bg-gray-500 relative md:w-96 max-md:w-48">
+                <div
+                  className="absolute tracking-wider text-sm bg-white px-4 py-2 font-bold text-center 
+              top-3 right-3 hover:bg-stone-500 cursor-pointer max-md:text-sm"
+                >
+                  On sale.
+                </div>
+                <div className="absolute bottom-4 right-3 left-3 bg-slate-50 px-4 py-2 shadow-sm uppercase text-center hover:bg-stone-500 cursor-pointer max-md:text-sm">
+                  Expand our product
+                </div>
+              </div>
+            </a>
+          </div>
+          <h3 className=" cursor-pointer md:text-center">
+            <a className="text-slate-500 font-semi-bold text-base ">
+              Pink Preminum Ceramic
+            </a>
+            <p className="text-slate-500 font-thin text-sm max-md:px-16 max-md:text-sm">
+              $23.00
+            </p>
+          </h3>
+        </div>
+      
        
       </div>
     </div>
--- src/One/components/Story.js
+++ src/One/components/Story.js
@@ -3,18 +3,19 @@ function Story() {
   let openDiv = document.getElementById("open-story");
 
   function handleFullContent() {
-    let fullStoryDiv = document.getElementById("full-story");
+   fullStoryDiv = document.getElementById("full-story");
     fullStoryDiv.classList.toggle("hidden");
     fullStoryDiv.classList.toggle("my-full-story");
-     openDiv.classList.toggle("opacity-95");
-    
+    openDiv.classList.toggle("opacity-95");
   }
   return (
     <div className="w-full h-full flex justify-center items-center">
       <div className="text-center px-32 py-32 flex flex-col gap-y-8">
         <div className="text-3xl text-center font-Karla">
           Even the all-powerful Pointing has
-          <span className="block font-Karla">no control about the blind texts.</span> 
+          <span className="block font-Karla">
+            no control about the blind texts.
+          </span>
         </div>
         <div className="text-gray-500 leading-7 mb-6">
           It is a paradisematic country, in which roasted parts of sentences fly
@@ -29,10 +30,7 @@ function Story() {
             className="my-8 cursor-pointer inline-block text-light-coffee opacity-50 hover:opacity-95 border-b-2 border-0 border-solid border-light-coffee"
             onClick={handleFullContent}
           >
-            <a className="no-underline">
-               Read the full story
-            </a>
-           
+            Read the full story
           </div>
 
           <div id="full-story" className="hidden text-gray-500 leading-7 mb-6">
